d16b685304fb0a876d6a8ac427fd81e4a19e1ad6
AntonioCastizo <antoniocastizo99@gmail.com>
2026-05-19 10:44:57 +0200

    fix php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 
-header('Content-Type', 'application/json');
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
+header('Content-Type: application/json');
 
 function conectarDB() {
     $DB_HOST = 'localhost';
